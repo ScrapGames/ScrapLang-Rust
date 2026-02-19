1f07c5af0c5ea9a7339e817ce99779256e7b9abe
Raxabi <92801687+Raxabi@users.noreply.github.com>
2026-02-20 00:39:01 +0100

    feat: added inline attrs for "Token" methods

--- src/frontend/tokens/mod.rs
+++ src/frontend/tokens/mod.rs
@@ -16,26 +16,32 @@ impl Token {
         Self { kind, content, position }
     }
 
+    #[inline]
     pub fn is_identifier(&self) -> bool {
         matches!(self.kind, Tokens::Identifier)
     }
 
+    #[inline]
     pub fn is_keyword(&self) -> bool {
         matches!(self.kind, Tokens::Keyword(_))
     }
 
+    #[inline]
     pub fn is_literal(&self) -> bool {
         matches!(self.kind, Tokens::Literal(_))
     }
 
+    #[inline]
     pub fn is_operator(&self) -> bool {
         matches!(self.kind, Tokens::Operator(_))
     }
 
+    #[inline]
     pub fn is(&self, kind: Tokens) -> bool {
         self.kind == kind
     }
 
+    #[inline]
     pub fn cmp(&self, content: &str) -> bool {
         self.content == content
     }
